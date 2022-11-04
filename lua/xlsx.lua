4a1f2caa658f2acfb7c6e4660c4dca77d0eca25f
愤怒的蜂鸟 <xjqxz_2@163.com>
2022-11-04 14:10:50 +0800

    In XLSX.lua has bugs

--- lua/xlsx.lua
+++ lua/xlsx.lua
@@ -2,7 +2,7 @@ local ffi = require("ffi")
 
 --  Load shex fii lib
 local shex = ffi.load("shex")
-ffi.cdef[[
+ffi.cdef [[
 typedef struct { const char *p; ptrdiff_t n; } _GoString_;
 typedef char _check_for_64_bit_pointer_matching_GoInt[sizeof(void*)==64/8 ? 1:-1];
 typedef signed char GoInt8;
@@ -45,11 +45,11 @@ extern GoInt setPageMargins(GoInt resourceId, GoFloat64 top, GoFloat64 left, GoF
 
 local Excel = {
     resourceId = 0,
-    filename  = ""
+    filename = ""
 }
 
 -- Golang String 
-function GoString(s) 
+function GoString(s)
     local gstr = ffi.new("GoString")
 
     gstr.p = s
@@ -58,98 +58,96 @@ function GoString(s)
     return gstr
 end
 
-
-
 --  创建一个 Excel 文件
-function Excel:new(filename,defaultSheetName) 
+function Excel:new(filename, defaultSheetName)
     o = o or {}
     setmetatable(o, self)
     self.__index = self
     self.filename = filename
 
-    if #defaultSheetName <= 0 then 
+    if #defaultSheetName <= 0 then
         defaultSheetName = "Sheet1"
-    end 
+    end
 
-    self.resourceId = shex.createExcelFile(GoString(filename),GoString(defaultSheetName))
+    self.resourceId = shex.createExcelFile(GoString(filename), GoString(defaultSheetName))
     return o
-end 
+end
 
 --  选择一个工作表 默认为工作表名为 sheet1
-function Excel:sel(sheet) 
-    shex.selectSheet(self.resourceId ,GoString(sheet))
+function Excel:sel(sheet)
+    shex.selectSheet(self.resourceId, GoString(sheet))
     return o
 end
 
 --  向工作区域中填充数据
-function Excel:cell(index ,value) 
+function Excel:cell(index, value)
     shex.cell(
-        self.resourceId ,
-        GoString(index), 
-        GoString(value)
+            self.resourceId,
+            GoString(index),
+            GoString(value)
     )
 
     return o
 end
 
-function Excel:merge(si ,ei) 
-    shex.merge(self.resourceId,GoString(si),GoString(ei))
+function Excel:merge(si, ei)
+    shex.merge(self.resourceId, GoString(si), GoString(ei))
     return o
 end
 
 --  保存数据表
-function Excel:save() 
+function Excel:save()
     shex.save(self.resourceId)
 end
 
 --  注册Excel表样式
 --  样式表请查阅这里 https://xuri.me/excelize/zh-hans/style.html 
-function Excel:registerStyle(style) 
+function Excel:registerStyle(style)
     return shex.registerStyle(
-        self.resourceId,
-        GoString(style)
+            self.resourceId,
+            GoString(style)
     )
-end 
+end
 
 --  设置单元格样式
-function Excel:setCellStyle(cellIndexX ,cellIndexY,styleId)
+function Excel:setCellStyle(cellIndexX, cellIndexY, styleId)
     shex.setCellStyle(
-        self.resourceId, 
-        GoString(cellIndexX),
-        GoString(cellIndexY),
-        tonumber(styleId)
+            self.resourceId,
+            GoString(cellIndexX),
+            GoString(cellIndexY),
+            tonumber(styleId)
     )
 end
 
 --  设置列宽
-function Excel:setColWidth(startCol ,endCol ,width) 
+function Excel:setColWidth(startCol, endCol, width)
     --  设置 A 列 shex.setColWidth(self.resourceId,GoString("A"),GoString("A"),50.1)
     --  设置 A ~ C 列 shex.setColWidth(self.resourceId,GoString("A"),GoString("C"),50.1)
-    shex.setColWidth(self.resourceId,GoString("A"),GoString("A"),50.1)
+    shex.setColWidth(self.resourceId, GoString(startCol), GoString(endCol), width)
 end
 
 --  设置行高
-function Excel:setRowHeight(row ,height) 
-    shex.setRowHeight(self.resourceId,3,20.1)
+function Excel:setRowHeight(row, height)
+    shex.setRowHeight(self.resourceId, row, height)
 end
 
 -- 插入分页符
-function Excel:insertPageBreak(cell) 
-    shex.insertPageBreak(self.resourceId,GoString(cell))
+function Excel:insertPageBreak(cell)
+    shex.insertPageBreak(self.resourceId, GoString(cell))
 end
 
 -- 设置列样式
-function Excel:setColStyle(columns ,styleId) 
-    shex.setColStyle(self.resourceId,GoString(columns),tonumber(styleId))
+function Excel:setColStyle(columns, styleId)
+    shex.setColStyle(self.resourceId, GoString(columns), tonumber(styleId))
 end
 
 -- 获取单元格样式
 function Excel:getCellStyle(axis)
-    shex.getCellStyle(self.resourceId,GoString(axis))
+    shex.getCellStyle(self.resourceId, GoString(axis))
 end
 
 -- 追加单元格线条样式
-function Excel:appendBoardStyle(board ,axis)
+function Excel:appendBoardStyle(board, axis)
     return shex.appendBoardStyle(self.resourceId, GoString(board), GoString(axis))
 end
 
@@ -157,7 +155,7 @@ end
 -- 若不想修改某一个值的默认值请将其值设置为 -1 ,参数都应该是小数点（Float64)
 -- 如 Excel:setPageMargins(-1.0, 1.0, 0.0, 1.2, 2.0, 19.0)
 function Excel:setPageMargins(top, left, right, bottom, header, footer)
-    return shex.setPageMargins(self.resourceId,top, left, right, bottom, header, footer)
-end 
+    return shex.setPageMargins(self.resourceId, top, left, right, bottom, header, footer)
+end
 
 return Excel
